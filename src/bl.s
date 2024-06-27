fa49db3dbef581e9fe7d16cee26657a10f3236d3
thomas-koon <tkoon@ucsd.edu>
2024-06-27 10:03:48 -0700

    erase success for blinky. needs test with larger images

--- src/bl.s
+++ src/bl.s
@@ -11,11 +11,23 @@
 .equ LED_PIN, 5  /* PA5 */
 
 .equ FLASH_BASE_ADDR, 0x40023C00
-.equ FLASH_KEYR_OFFSET,    0x04
+.equ FLASH_KEYR_ADDR, 0x40023C04    /* Offset 0x04 */
+.equ FLASH_CR_ADDR, 0x40023C10      /* Offset 0x10 */
+.equ FLASH_SR_ADDR, 0x40023C0C      /* Offset 0x0C */
+
 .equ FLASH_KEY1, 0x45670123
 .equ FLASH_KEY2, 0xCDEF89AB
-.equ FLASH_CR_OFFSET, 0x10
-.equ FLASH_CR_LOCK_BIT, 0x80000000
+
+.equ FLASH_CR_LOCK, 0x80000000
+.equ FLASH_CR_STRT, 0x00010000
+.equ FLASH_CR_SER, 0x00000002
+.equ FLASH_CR_PG, 0x01
+
+.equ FLASH_SR_BSY, 0x00010000
+
+.equ BOOT_SECTOR_ADDRESS, 0x8004000
+.equ UPDATE_SECTOR_ADDRESS, 0x8040000
+.equ UPDATE_SECTOR_SIZE, 0x3E800
 
 .section  .isr_vector,"a",%progbits
 .type  g_pfnVectors, %object
@@ -33,13 +45,16 @@ Reset_Handler:
     bl Unlock_Flash
     bl Lock_Flash
     bl Unlock_Flash
+
+    bl Erase_Boot
+
     bl Lock_Flash
 
     /* jump to application: set MSP to app's vector table */
     /* commenting this part out should have LD2 (green right side LED) stay on */
-    ldr r13, =0x08004000     /* boot MSP */
-    ldr r0, =0x080048a5      /* reset handler address (verified in gdb disas)*/  
-    bx r0     /* boot reset handler */
+    // ldr r13, =0x08004000     /* boot MSP */
+    // ldr r0, =0x080048a5      /* reset handler address (verified in gdb disas)*/  
+    // bx r0     /* boot reset handler */
 
     /* Enable GPIOA clock */
     ldr r0, =RCC_AHB1ENR  /* Load RCC_AHB1ENR address into r0 */
@@ -65,10 +80,8 @@ Reset_Handler:
     str r1, [r0]           /* Turn on LED (set PA5) */
     
 Unlock_Flash:
-    /* Load the base address of the FLASH registers */
-    ldr     r0, =FLASH_BASE_ADDR
-    /* Load the address of the FLASH_KEYR register */ 
-    add     r0, r0, #FLASH_KEYR_OFFSET
+    /* Load the address of the FLASH_KEYR register */
+    ldr     r0, =FLASH_KEYR_ADDR
     
     /* Write the first key to the FLASH_KEYR */ 
     LDR     r1, =FLASH_KEY1
@@ -82,17 +95,87 @@ Unlock_Flash:
     bx      lr
 
 Lock_Flash:
-    /* Load the base address of the FLASH registers */
-    ldr     r0, =FLASH_BASE_ADDR
     /* Load the address of the FLASH_CR register */
-    add     r0, r0, #FLASH_CR_OFFSET
+    ldr     r0, =FLASH_CR_ADDR
     
     /* Read the current value of the FLASH_CR register */
     ldr     r1, [r0]
     /* Set the LOCK bit */
-    orr     r1, r1, #FLASH_CR_LOCK_BIT
+    orr     r1, r1, #FLASH_CR_LOCK
     /* Write the modified value back to the FLASH_CR register */
     str     r1, [r0]
     
     /* Return from the function */
     bx      lr
+
+Erase_Boot:
+    /* erase Sectors 1, 2, 3, 4, 5 */
+    mov r0, #(1 << 3)     /* Sector 1 (aligned to bits 6:3) */
+    bl Erase_Sector
+    mov r0, #(2 << 3)     /* Sector 2 (aligned to bits 6:3) */
+    bl Erase_Sector
+    mov r0, #(3 << 3)     /* Sector 3 (aligned to bits 6:3) */
+    bl Erase_Sector
+    mov r0, #(4 << 3)     /* Sector 4 (aligned to bits 6:3) */
+    bl Erase_Sector
+    mov r0, #(5 << 3)     /* Sector 5 (aligned to bits 6:3) */
+    bl Erase_Sector
+    bx lr          /* Return from function */
+
+Erase_Sector:
+    /* Input: r0 = sector number */
+    /* Load the address of the FLASH_CR register */
+    ldr r1, =FLASH_CR_ADDR
+
+    /* Set the SER bit and the sector number in FLASH_CR */
+    ldr r2, [r1]
+    orr r2, r2, #FLASH_CR_SER /* set SER bit */
+    bic r2, r2, #(0xF << 3)  /* Clear sector number bits */
+    orr r2, r2, r0   /* Set sector number */
+    str r2, [r1]
+    
+    /* Set the START bit */
+    orr r2, r2, #FLASH_CR_STRT
+    str r2, [r1]
+
+Wait_Busy:
+    /* Wait for the BSY bit to be cleared in FLASH_SR */
+    ldr r1, =FLASH_SR_ADDR
+    ldr r2, [r1]            /* Read the value of FLASH_SR */
+    tst r2, #FLASH_SR_BSY   /* Test the BSY bit in FLASH_SR */
+    bne Wait_Busy
+    bx lr
+
+Copy_Update:
+    ldr r2, =UPDATE_SECTOR_ADDRESS
+    ldr r3, =BOOT_SECTOR_ADDRESS
+    ldr r4, =UPDATE_SECTOR_SIZE
+
+Copy_Loop:
+    cmp r4, #0      /* If update sector size copied */
+    bx lr
+
+    ldr r1, [r2]    /* Read value of update sector address */
+
+Wait_Busy_Copy:
+    /* check BSY bit in FLASH_SR */
+    ldr r0, =FLASH_SR_ADDR
+    ldr r5, [r0]
+    tst r5, #FLASH_SR_BSY
+    bne Wait_Busy_Copy
+
+    /* set PG bit in FLASH_CR */
+    ldr r0, =FLASH_CR_ADDR
+    ldr r5, [r0]
+    orr r5, r5, #FLASH_CR_PG
+    str r5, [r0]
+
+    /* Write the word to the boot sector */
+    /* r1 is the value at current update sector address */
+    str r1, [r3]
+
+    /* loop */
+    add r2, r2, #4
+    add r3, r3, #4
+    sub r4, r4, #4
+    b Copy_Loop
